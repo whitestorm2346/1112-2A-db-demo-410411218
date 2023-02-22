164713369e8983aa72473f872b94cd0352e563e2
whitestorm2346 <andrew770426@gmail.com>
2023-02-22 15:02:46 +0800

    W02-P3: 建立路由 /crown_18 顯示 crown_18/index.ejs

---
+++ utils/database.js
@@ -0,0 +1,13 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    port: '5432',
+    database: 'crown_18',
+    password: '0000',
+});
+
+console.log('pool', pool.options.database);
+
+module.exports = pool;
