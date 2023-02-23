97ecd05aefb2fd3bad3c73010187aa1470dcff07
whitestorm2346 <andrew770426@gmail.com>
2023-02-23 16:45:27 +0800

    W02-P4: create table crown_18 using SQL commands

---
+++ utils/data/category_18.sql
@@ -0,0 +1,8 @@
+CREATE TABLE category_18 (
+    id int NOT NULL PRIMARY KEY,
+    name varchar(255),
+    size varchar(20),
+    local_url varchar(255),
+    remote_url varchar(255),
+    link_url varchar(255)
+)
