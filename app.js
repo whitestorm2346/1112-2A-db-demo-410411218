9a1c115d5b40288496d9e342d808d128dec2e909
whitestorm2346 <andrew770426@gmail.com>
2023-04-26 14:13:14 +0800

    W11-P2: Connecting Supabase

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const dotenv = require('dotenv');
+dotenv.config();
+
 const db = require('./utils/database');
 // const testDB = require('./utils/test-db');
 
--- utils/database.js
+++ utils/database.js
@@ -1,14 +1,29 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    // port: '5433', // at school
-    port: '5432', // at home
-    database: 'crown_18',
-    password: '0000',
-});
+let pool;
 
-console.log(`Postgres server running on ${pool.options.database} database`);
+if(process.env.DATABASE === 'SUPABASE'){
+    pool = new Pool({
+        user: 'postgres',
+        host: process.env.SUPABASE_HOST,
+        port: '5432',
+        database: 'postgres',
+        password: process.env.PASSWORD,
+    });
+
+    console.log(`Connecting Supabase PostgreSQL running on ${pool.options.database} database`);
+}
+else {
+    pool = new Pool({
+        user: 'postgres',
+        host: 'localhost',
+        // port: '5433', // at school
+        port: '5432', // at home
+        database: 'crown_18',
+        password: '0000',
+    });
+
+    console.log(`Connecting local PostgreSQL running on ${pool.options.database} database`);
+}
 
 module.exports = pool;
